eaed9a675f92dcc186bb8af7f07a68ef0cea7d06
Shunsuke Kimura <kimushun1101@gmail.com>
2020-09-24 00:01:17 +0900

    add teleop

--- launch/my_script.launch.py
+++ launch/my_script.launch.py
@@ -11,6 +11,8 @@ from launch_ros.actions import Node
 roomba_launch_file_dir = os.path.join(get_package_share_directory('roomba_600_driver'), 'launch')
 darknet_launch_file_dir = os.path.join(get_package_share_directory('darknet_ros'), 'launch')
 test_launch_file_dir = os.path.join(get_package_share_directory('test_pkg'), 'launch')
+teleop_launch_file_dir = os.path.join(get_package_share_directory('teleop_twist_joy'), 'launch')
+cbf_launch_file_dir = os.path.join(get_package_share_directory('cbf_assist'), 'launch')
 
 def generate_launch_description():
     return launch.LaunchDescription([
@@ -25,4 +27,10 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([test_launch_file_dir, '/melody.launch.py']),
         ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([teleop_launch_file_dir, '/teleop-launch.py']),
+        ),
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([cbf_launch_file_dir, '/controller_with_teleop_twist_joy_launch.py']),
+        # ),
     ])
